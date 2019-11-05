a51731a0b16d747327017c8e99e6c9883d13fdec
KatieTooher <katie.tooher@marquette.edu>
2019-11-05 17:34:14 -0600

    changes to gwc and removed large files

---
+++ code/StudentClass.cs
@@ -0,0 +1,81 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Student katie = new Student("Katie",20,15,"Computer Science", "sand");
+
+    Console.WriteLine(katie.ToString());
+    
+  }
+}
+public class Student{
+      string name,sub,al;
+      int age, grade;
+
+  //default constructor
+  public Student(){
+    name = "Student";
+    grade = 9;
+    age = 14;
+    sub = "School";
+    al = "N/A";
+  }
+  //constructor
+  public Student(string name, int age, int grade, string sub, string al){
+      this.name = name;
+      this.age = age;
+      this.grade =  grade;
+      this.sub = sub;
+      this.al = al;
+  }
+
+  //sets 
+  public void setName(string n){
+    this.name = n;
+  }
+
+  public void setGrade(int g){
+    this.grade = g;
+  }
+
+  public void setAge(int a){
+    this.age = a;
+  }
+
+  public void setSub(string sub){
+    this.sub = sub;
+  }
+
+  public void setAllergies(string al){
+    this.al = al;
+  }
+
+
+  ///gets
+  public string getName(){
+    return this.name;
+  }
+
+  public string getSub(){
+    return this.sub;
+  }
+
+  public string getAllergies(){
+    return this.al;
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public int getGrade(){
+    return this.grade;
+  }
+
+  public override string ToString(){
+    //tostring method will print the object
+    return $"{name} is {age} years old in grade {grade} and have favorite subject {sub} and are allergic to {al}";
+  }
+
+
+}
